0b15025df6dfb7264ebc75cc4704e91f89f1b5d8
Ardiv26 <ardivdev89@hotmail.com>
2025-02-13 18:02:19 +0900

    rename as Ardiv

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import {
   FaHtml5,
@@ -6,183 +6,183 @@ import {
   FaJs,
   FaReact,
   FaFigma,
-  FaNodeJs,
-} from "react-icons/fa";
-import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+  FaNodeJs
+} from 'react-icons/fa';
+import { SiTailwindcss, SiNextdotjs } from 'react-icons/si';
 
 // about data
 const about = {
-  title: "About me",
+  title: 'About me',
   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident assumenda veritatis ratione cumque maiores, qui pariatur sint itaque.",
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident assumenda veritatis ratione cumque maiores, qui pariatur sint itaque.',
   info: [
     {
-      fieldName: "Name",
-      fieldValue: "Ross Geller",
+      fieldName: 'Name',
+      fieldValue: 'Ardiv Johar Bin Johar'
     },
     {
-      fieldName: "Phone",
-      fieldValue: "(+86) 123 456 789",
+      fieldName: 'Phone',
+      fieldValue: '(+86) 123 456 789'
     },
     {
-      fieldName: "Experience",
-      fieldValue: "12+ Years",
+      fieldName: 'Experience',
+      fieldValue: '12+ Years'
     },
     {
-      fieldName: "Skype",
-      fieldValue: "yourskype",
+      fieldName: 'Skype',
+      fieldValue: 'yourskype'
     },
     {
-      fieldName: "Nationality",
-      fieldValue: "Chinese",
+      fieldName: 'Nationality',
+      fieldValue: 'Chinese'
     },
     {
-      fieldName: "Email",
-      fieldValue: "youremail@email.com",
+      fieldName: 'Email',
+      fieldValue: 'youremail@email.com'
     },
     {
-      fieldName: "Freelance",
-      fieldValue: "Available",
+      fieldName: 'Freelance',
+      fieldValue: 'Available'
     },
     {
-      fieldName: "Languages",
-      fieldValue: "English, Chinese",
-    },
-  ],
+      fieldName: 'Languages',
+      fieldValue: 'English, Chinese'
+    }
+  ]
 };
 
 // experience data
 const experience = {
-  icon: "/assets/resume/badge.svg",
-  title: "My experience",
+  icon: '/assets/resume/badge.svg',
+  title: 'My experience',
   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident assumenda veritatis ratione cumque maiores, qui pariatur sint itaque.",
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident assumenda veritatis ratione cumque maiores, qui pariatur sint itaque.',
   items: [
     {
-      company: "Tech Solutions Inc.",
-      position: "Full Stack Developer",
-      duration: "2022 - Present",
+      company: 'Tech Solutions Inc.',
+      position: 'Full Stack Developer',
+      duration: '2022 - Present'
     },
     {
-      company: "Web Design Studio",
-      position: "Front-End Developer Intern",
-      duration: "Summer 2021",
+      company: 'Web Design Studio',
+      position: 'Front-End Developer Intern',
+      duration: 'Summer 2021'
     },
     {
-      company: "E-commerce Startup",
-      position: "Freelance Web Developer",
-      duration: "2020 - 2021",
+      company: 'E-commerce Startup',
+      position: 'Freelance Web Developer',
+      duration: '2020 - 2021'
     },
     {
-      company: "Tech Academy",
-      position: "Teaching Assistant",
-      duration: "2019 - 2020",
+      company: 'Tech Academy',
+      position: 'Teaching Assistant',
+      duration: '2019 - 2020'
     },
     {
-      company: "Digital Agency",
-      position: "UI/UX Designer",
-      duration: "2018 - 2019",
+      company: 'Digital Agency',
+      position: 'UI/UX Designer',
+      duration: '2018 - 2019'
     },
     {
-      company: "Software Development Firm",
-      position: "Junior Developer",
-      duration: "2017 - 2018",
-    },
-  ],
+      company: 'Software Development Firm',
+      position: 'Junior Developer',
+      duration: '2017 - 2018'
+    }
+  ]
 };
 
 // education data
 const education = {
-  icon: "/assets/resume/cap.svg",
-  title: "My education",
+  icon: '/assets/resume/cap.svg',
+  title: 'My education',
   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident assumenda veritatis ratione cumque maiores, qui pariatur sint itaque.",
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident assumenda veritatis ratione cumque maiores, qui pariatur sint itaque.',
   items: [
     {
-      institution: "Online Course Platform",
-      degree: "Full Stack Web Development Bootcamp",
-      duration: "2023",
+      institution: 'Online Course Platform',
+      degree: 'Full Stack Web Development Bootcamp',
+      duration: '2023'
     },
     {
-      institution: "Codecademy",
-      degree: "Front-end Track",
-      duration: "2022",
+      institution: 'Codecademy',
+      degree: 'Front-end Track',
+      duration: '2022'
     },
     {
-      institution: "Online Course",
-      degree: "Programming Course",
-      duration: "2020 - 2021",
+      institution: 'Online Course',
+      degree: 'Programming Course',
+      duration: '2020 - 2021'
     },
     {
-      institution: "Tech Institute",
-      degree: "Certified Web Developer",
-      duration: "2019",
+      institution: 'Tech Institute',
+      degree: 'Certified Web Developer',
+      duration: '2019'
     },
     {
-      institution: "Design School",
-      degree: "Diploma in Graphic Design",
-      duration: "2016 - 2018",
+      institution: 'Design School',
+      degree: 'Diploma in Graphic Design',
+      duration: '2016 - 2018'
     },
     {
-      institution: "Community Collage",
-      degree: "Associate Degree in Computer Science",
-      duration: "2014 - 2016",
-    },
-  ],
+      institution: 'Community Collage',
+      degree: 'Associate Degree in Computer Science',
+      duration: '2014 - 2016'
+    }
+  ]
 };
 
 // skills data
 const skills = {
-  title: "My skills",
+  title: 'My skills',
   description:
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad rem beatae eius aut, nam adipisci deserunt perspiciatis sequi atque.",
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad rem beatae eius aut, nam adipisci deserunt perspiciatis sequi atque.',
   skillList: [
     {
       icon: <FaHtml5 />,
-      name: "html 5",
+      name: 'html 5'
     },
     {
       icon: <FaCss3 />,
-      name: "css 3",
+      name: 'css 3'
     },
     {
       icon: <FaJs />,
-      name: "javascript",
+      name: 'javascript'
     },
     {
       icon: <FaReact />,
-      name: "react.js",
+      name: 'react.js'
     },
     {
       icon: <SiNextdotjs />,
-      name: "next.js",
+      name: 'next.js'
     },
     {
       icon: <SiTailwindcss />,
-      name: "tailwind.css",
+      name: 'tailwind.css'
     },
     {
       icon: <FaNodeJs />,
-      name: "node.js",
+      name: 'node.js'
     },
     {
       icon: <FaFigma />,
-      name: "figma",
-    },
-  ],
+      name: 'figma'
+    }
+  ]
 };
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+  TooltipTrigger
+} from '@/components/ui/tooltip';
 
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { motion } from "framer-motion";
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { motion } from 'framer-motion';
 
 const Resume = () => {
   return (
@@ -190,7 +190,7 @@ const Resume = () => {
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+        transition: { delay: 2.4, duration: 0.4, ease: 'easeIn' }
       }}
       className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
     >
